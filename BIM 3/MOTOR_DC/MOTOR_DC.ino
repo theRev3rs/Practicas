65b18f2724ff17c04648a2946f8de6e18102f9a3
Ricardo Estrada <86979079+theRev3rs@users.noreply.github.com>
2023-06-07 20:23:10 -0600

    Add files via upload

---
+++ BIM 3/MOTOR_DC/MOTOR_DC.ino
@@ -0,0 +1,54 @@
+//Directivas de Preprocesador
+#define pinA 6
+#define pinB 5
+#define clockwise 2
+#define anticlockwise 3
+#define duplicar_rpm 8
+//variables
+int rpm_final;
+int rpm_fijo = 120;
+bool clockwiselock;
+bool anticlockwiselock;
+
+//FUNCIONES
+void funcion_ISR_clockwise(void);
+void funcion_ISR_anticlockwise(void);
+
+void setup() {
+  Serial.begin(9600);
+pinMode(pinA, OUTPUT);
+pinMode(pinB, OUTPUT);
+pinMode(clockwise, INPUT);
+pinMode(anticlockwise, INPUT);
+pinMode(duplicar_rpm, INPUT);
+attachInterrupt(digitalPinToInterrupt(clockwise),funcion_ISR_clockwise,RISING); //Anclar la interrupcion del pin 2 a una Funcion ISR disparada por el flanco ascendente
+attachInterrupt(digitalPinToInterrupt(anticlockwise),funcion_ISR_anticlockwise,RISING); //Anclar la interrupcion del pin 2 a una Funcion ISR disparada por el flanco ascendente
+}
+
+void loop() {
+   if(digitalRead(duplicar_rpm) == HIGH){
+     rpm_final = 255;
+   }
+   else{
+     rpm_final = rpm_fijo;
+   }
+   
+  if(clockwiselock == HIGH && anticlockwiselock == LOW){
+    analogWrite(pinA, rpm_final);
+    digitalWrite(pinB, LOW);
+    }
+  if(clockwiselock == LOW && anticlockwiselock == HIGH){
+    digitalWrite(pinA, LOW);
+    analogWrite(pinB, rpm_final);
+    }
+}
+  void funcion_ISR_clockwise(){  //Cuando se dispara la interrupcion por Rising
+    Serial.println("movimiento horario");
+   clockwiselock=HIGH;
+   anticlockwiselock=LOW;
+}
+  void funcion_ISR_anticlockwise(){  //Cuando se dispara la interrupcion por Rising
+    Serial.println("movimiento antihorario");
+   clockwiselock=LOW;
+   anticlockwiselock=HIGH;
+}
