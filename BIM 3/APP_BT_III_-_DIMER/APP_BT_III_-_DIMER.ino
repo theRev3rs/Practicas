d1d77a51047ba59aaf83fc8055a99191cec15a35
Ricardo Estrada <86979079+theRev3rs@users.noreply.github.com>
2023-07-26 14:37:42 -0600

    Add files via upload

--- BIM 3/APP_BT_III_-_DIMER/APP_BT_III_-_DIMER.ino
+++ BIM 3/APP_BT_III_-_DIMER/APP_BT_III_-_DIMER.ino
@@ -13,11 +13,11 @@
  */
 
 //Librerias
-#include <Ticker.h>
+
 
 //Directivas
-#define dimerOut 3
-#define dimerOn 6
+#define dimerOut 5
+#define cruce_cero 2
 
 //Variables
 byte intensidad_foco;
@@ -32,16 +32,16 @@ byte dimersignal = 0;
 void potencia_foco();
 
 //Constructores
- Ticker ISR_cero(potencia_foco, 16);
+ 
 
 void setup() {
+  attachInterrupt(digitalPinToInterrupt(cruce_cero),potencia_foco,RISING);
   Serial.begin(9600);
   pinMode(dimerOut, OUTPUT);
-  ISR_cero.start();
+  pinMode(cruce_cero, INPUT);
 }
 
 void loop() {
-  ISR_cero.update();
   if(Serial.available()>0 && btconectado == false){
     entradabt = Serial.readStringUntil('\n');
       if(entradabt == String("Conectado")){
@@ -50,27 +50,22 @@ void loop() {
     }
   if(Serial.available()>0 && btconectado == true){
     intensidad_foco = Serial.parseInt();
-    dimersignal = map(intensidad_foco, 0, 255, 1, 8);
-      if(intensidad_foco > 0){
-        digitalWrite(dimerOn, HIGH);
-        }
-      if(intensidad_foco == 0){
-        digitalWrite(dimerOn, LOW);
-        }
-        if(dimersignal == 1 ) luz_int = 6300;   // Los 8 saltos del Deslizador 1 (Dimmer)
+    dimersignal = map(intensidad_foco, 0, 255, 0, 8);
+        if(dimersignal == 0 ) luz_int = 7600; 
+        if(dimersignal == 1 ) luz_int = 6900;
         if(dimersignal == 2 ) luz_int = 6100;  
         if(dimersignal == 3 ) luz_int = 5200;  
         if(dimersignal == 4 ) luz_int = 4900;  
         if(dimersignal == 5 ) luz_int = 4500; 
         if(dimersignal == 6 ) luz_int = 4100; 
         if(dimersignal == 7 ) luz_int = 3500;  
-        if(dimersignal == 8 ) luz_int = 3000; 
+        if(dimersignal == 8 ) luz_int = 3000;
     }
 }
 
 void potencia_foco(){
   delayMicroseconds(luz_int); 
-  digitalWrite(3,HIGH);
+  digitalWrite(dimerOut,HIGH);
   delayMicroseconds(100);
-  digitalWrite(3,LOW);
+  digitalWrite(dimerOut,LOW);
   }
